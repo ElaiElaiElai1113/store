1b73aad21dac58afa014c3ca19ef0162fa4ae5d1
ElaiElaiElai1113 <ensdelossantos@addu.edu.ph>
2024-09-24 19:37:12 +0800

    efeokf

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import FilterProductTable from "./components/FilterProductTable";
 import SearchBar from "./components/SearchBar";
 import ProductTable from "./components/ProductTable";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const products = [
+    //Sporting Goods
+    { id: 1, name: "Tennis", price: 99.9, type: 1 },
+    { id: 2, name: "Badminton", price: 59.9, type: 1 },
+    { id: 3, name: "Basketball", price: 100, type: 1 },
+    { id: 4, name: "Soccer", price: 65, type: 1 },
+    //Electronics
+    { id: 5, name: "IPod Touch", price: 99.99, type: 2 },
+    { id: 6, name: "IPhone 5", price: 399.9, type: 2 },
+    { id: 7, name: "Nexus 7", price: 199.9, type: 2 },
+  ];
 
+  const headers = ["Sporting Goods", "Electronics", "TOTAL"];
   return (
     <>
       <FilterProductTable>
         <SearchBar />
-        <ProductTable />
+        <ProductTable headers={headers} products={products} />
       </FilterProductTable>
     </>
   );
